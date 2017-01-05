767f8be8ebd7ca0f77dd24158394c15381234fc0
joecatanese <joseph.catanese@mhslead.org>
2017-01-05 14:46:50 -0700

    Add files via upload

--- team5.py
+++ team5.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Joe and Gill' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
@@ -65,4 +65,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')     
